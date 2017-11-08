82ce5a96507063e72681af10b135153ae7cf627a
J C <jc@Js-MacBook-Pro.gateway>
2017-11-08 22:45:30 +1100

    chuck this away later

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,40 @@
-$(document).ready(function() {
-  $("#addButton").on("click", function() {
-    $.getJSON("http://fakerestapi.azurewebsites.net", function(json) {
-      $(".message").html(JSON.stringify(json));
-    });
+quote();
+
+function quote() {
+  $.ajax({
+    url: "http://api.forismatic.com/api/1.0/",
+    jsonp: "jsonp",
+    dataType: "jsonp",
+    data: {
+      method: "getQuote",
+      lang: "en",
+      format: "jsonp"
+    },
+    success: function(response) {
+      $('#quote').html(response.quoteText)
+      $('#author').html(response.quoteAuthor)
+    }
   });
+}
+
+$("#quoteButton").on("click", function() {
+  quote();
 });
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
