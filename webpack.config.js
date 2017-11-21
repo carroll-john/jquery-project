aa31a5c5eb4f4669fe53c0b37a6b0c4fc138c46e
John Carroll <carroll_john@me.com>
2017-11-21 22:41:37 +1100

    start to add webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './js/scripts.js',
+  import 'bootstrap';
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
